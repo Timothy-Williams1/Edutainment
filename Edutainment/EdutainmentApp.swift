b9fef68e95f693488d8a803be105b4ca2308f4b8
Timothy Williams <Timothy.Williams@stu.glasgow.kyschools.us>
2024-11-11 09:10:13 -0600

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Student on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
